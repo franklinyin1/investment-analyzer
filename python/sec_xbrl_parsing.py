c9b913fab389183832e4166cb3d90934570cc55e
Franklin Yin <franklinyin1@gmail.com>
2021-11-05 17:05:00 -0400

    python SEC script

---
+++ python/sec_xbrl_parsing.py
@@ -0,0 +1,14 @@
+import csv
+import pprint
+import pathlib
+import collections
+import xml.etree.ElementTree as etree
+
+# define our working directory
+sec_directory = pathlib.Path.cwd().joinpath("facebook10Q")
+
+# define file paths to the documents
+file_htm = sec_directory.joinpath(TBU).resolve()
+file_cal = sec_directory.joinpath(TBU).resolve()
+file_lab = sec_directory.joinpath(TBU).resolve()
+file_def = sec_directory.joinpath(TBU).resolve()
