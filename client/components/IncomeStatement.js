8f2b8bab63f7d9487ca638e1b14699fe7f27568f
Franklin Yin <franklinyin1@gmail.com>
2021-12-06 15:25:13 -0500

    filter income statement

--- client/components/IncomeStatement.js
+++ client/components/IncomeStatement.js
@@ -18,22 +18,35 @@ class IncomeStatement extends React.Component {
         <td key={`cell${0}-6`}>Unit Of Measure</td>,
       ],
     ];
+
     if (company.financials) {
-      for (let i = 1; i < company.financials.length; i++) {
+
+      //filter financials to only include income statement items
+      let financials = company.financials.filter((financial) => {
+        let presentation = company.presentations.filter((presentation) => {
+          return presentation.adsh === financial.adsh && presentation.stmt === 'IS' && presentation.tag === financial.tag
+        })
+        // console.log('financial:', financial)
+        // console.log('adsh:', adsh)
+        // console.log('presentation:', presentation)
+        return presentation.length > 0
+      })
+
+      for (let i = 1; i < financials.length; i++) {
         let rowId = `row${i}`;
         let cell = [];
-        cell.push(<td key={`cell${i}-1`}>{company.financials[i - 1].tag}</td>);
+        cell.push(<td key={`cell${i}-1`}>{financials[i - 1].tag}</td>);
         cell.push(
-          <td key={`cell${i}-2`}>{company.financials[i - 1].version}</td>
+          <td key={`cell${i}-2`}>{financials[i - 1].version}</td>
         );
         cell.push(
-          <td key={`cell${i}-3`}>{company.financials[i - 1].ddate}</td>
+          <td key={`cell${i}-3`}>{financials[i - 1].ddate}</td>
         );
-        cell.push(<td key={`cell${i}-4`}>{company.financials[i - 1].qtrs}</td>);
+        cell.push(<td key={`cell${i}-4`}>{financials[i - 1].qtrs}</td>);
         cell.push(
-          <td key={`cell${i}-5`}>{company.financials[i - 1].value.toLocaleString()}</td>
+          <td key={`cell${i}-5`}>{financials[i - 1].value.toLocaleString()}</td>
         );
-        cell.push(<td key={`cell${i}-6`}>{company.financials[i - 1].uom}</td>);
+        cell.push(<td key={`cell${i}-6`}>{financials[i - 1].uom}</td>);
         rows.push(
           <tr key={i} id={rowId}>
             {cell}
