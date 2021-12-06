acc3af69bd6c2c9dc2630b22c48ae99a6579197f
Franklin Yin <franklinyin1@gmail.com>
2021-12-06 17:50:16 -0500

    balance sheet fix

--- client/components/CashFlowStatement.js
+++ client/components/CashFlowStatement.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-class BalanceSheet extends React.Component {
+class CashFlowStatement extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -13,37 +13,32 @@ class BalanceSheet extends React.Component {
         <td key={`cell${0}-1`}>Tag</td>,
         <td key={`cell${0}-2`}>Version</td>,
         <td key={`cell${0}-3`}>Period End Date</td>,
-        <td key={`cell${0}-4`}>Value</td>,
-        <td key={`cell${0}-5`}>Unit Of Measure</td>,
+        <td key={`cell${0}-4`}>Quarters</td>,
+        <td key={`cell${0}-5`}>Value</td>,
+        <td key={`cell${0}-6`}>Unit Of Measure</td>,
       ],
     ];
 
     if (company.financials) {
-      console.log('company.financials:', company.financials)
-      console.log('company.presentations:', company.presentations)
 
       //filter financials to only include income statement items
       let financials = company.financials.filter((financial) => {
         let presentation = company.presentations.filter((presentation) => {
-          return presentation.adsh === financial.adsh && presentation.stmt === 'BS'
+          return presentation.adsh === financial.adsh && presentation.stmt === 'IS'
         })
         return presentation.length > 0
       })
 
-      console.log('financials:', financials)
-
       //filter financials to only include current quarter
       let currentQuarter = '20210630'
       let currentQuarterFinancials = financials.filter((financial) => {
-        return financial.ddate === currentQuarter
+        return financial.ddate === currentQuarter && financial.qtrs === '1'
       })
-
-      console.log('currentQuarterFinancials:', currentQuarterFinancials)
 
       //add presentation detail as a key-value pair of each financial object
       currentQuarterFinancials = currentQuarterFinancials.map(financial => {
         let presentation = company.presentations.filter((presentation) => {
-          return presentation.adsh === financial.adsh && presentation.stmt === 'BS' && presentation.tag === financial.tag
+          return presentation.adsh === financial.adsh && presentation.stmt === 'IS' && presentation.tag === financial.tag
         })
         if (presentation.length > 0){
           financial.presentation = presentation
@@ -53,17 +48,11 @@ class BalanceSheet extends React.Component {
         return financial
       })
 
-      console.log('currentQuarterFinancials2:', currentQuarterFinancials)
-
       //sort the current quarter financials based on order of appearance in the income statement
       currentQuarterFinancials = currentQuarterFinancials.sort((x,y) => x.presentation[0].line - y.presentation[0].line)
 
-      console.log('currentQuarterFinancials3:', currentQuarterFinancials)
-
       //remove all current quarter financials without a specified line on the income statement
       currentQuarterFinancials = currentQuarterFinancials.filter((financial) => financial.presentation[0].line !== Infinity)
-
-      console.log('currentQuarterFinancials4:', currentQuarterFinancials)
 
       for (let i = 1; i < currentQuarterFinancials.length; i++) {
         let rowId = `row${i}`;
@@ -75,10 +64,11 @@ class BalanceSheet extends React.Component {
         cell.push(
           <td key={`cell${i}-3`}>{currentQuarterFinancials[i - 1].ddate}</td>
         );
+        cell.push(<td key={`cell${i}-4`}>{currentQuarterFinancials[i - 1].qtrs}</td>);
         cell.push(
-          <td key={`cell${i}-4`}>{currentQuarterFinancials[i - 1].value.toLocaleString()}</td>
+          <td key={`cell${i}-5`}>{currentQuarterFinancials[i - 1].value.toLocaleString()}</td>
         );
-        cell.push(<td key={`cell${i}-5`}>{currentQuarterFinancials[i - 1].uom}</td>);
+        cell.push(<td key={`cell${i}-6`}>{currentQuarterFinancials[i - 1].uom}</td>);
         rows.push(
           <tr key={i} id={rowId}>
             {cell}
@@ -91,7 +81,7 @@ class BalanceSheet extends React.Component {
       <React.Fragment>
         {rows.length > 1 ? (
           <React.Fragment>
-            <h3>Balance Sheet</h3>
+            <h3>Income Statement</h3>
             <table id="simple-board">
               <tbody>{rows}</tbody>
             </table>
@@ -108,4 +98,4 @@ class BalanceSheet extends React.Component {
  * CONTAINER
  */
 
-export default connect(null)(BalanceSheet);
+export default connect(null)(IncomeStatement);
