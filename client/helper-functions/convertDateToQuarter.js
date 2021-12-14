e7bc1f72934aa62cfa020ec2b1b8119f1beaeb8a
Franklin Yin <franklinyin1@gmail.com>
2021-12-14 18:00:17 -0500

    convertDateToFiscalPeriod helper function

---
+++ client/helper-functions/convertDateToQuarter.js
@@ -0,0 +1,35 @@
+function convertDateToFiscalPeriod(date, quarters) {
+  let year = date.slice(0, 4)
+  let month = date.slice(4, 6)
+  let day = date.slice(6, 8)
+  let fiscalPeriod
+  if (quarters === 1) {
+    if (month === 3) {
+      fiscalPeriod = 'Q1'
+    } else if (month === 6) {
+      fiscalPeriod = 'Q2'
+    } else if (month === 9) {
+      fiscalPeriod = 'Q3'
+    } else if (month === 12) {
+      fiscalPeriod = 'Q4'
+    }
+  } else if (quarters === 2) {
+    if (month === 6) {
+      fiscalPeriod = 'H1'
+    } else if (month === 12) {
+      fiscalPeriod = 'H2'
+    }
+  } else if (quarters === 4) {
+    fiscalPeriod = 'FY'
+  }
+
+  if (fiscalPeriod === 'FY') {
+    fiscalPeriod += year
+  } else {
+    fiscalPeriod = `${fiscalPeriod}'${year}`
+  }
+
+  return fiscalPeriod
+}
+
+export default convertDateToFiscalPeriod
