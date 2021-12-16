f575bc479471096a2d388780b3da73c7c7651464
Franklin Yin <franklinyin1@gmail.com>
2021-12-16 13:08:09 -0500

    remove legacy code from CoverPage

--- client/components/FinancialStatements/CoverPage.js
+++ client/components/FinancialStatements/CoverPage.js
@@ -34,49 +34,6 @@ class CoverPage extends React.Component {
     ];
 
     if (company.financials) {
-      // //filter financials to only include income statement items
-      // let financials = company.financials.filter((financial) => {
-      //   let presentation = company.presentations.filter((presentation) => {
-      //     return (
-      //       presentation.adsh === financial.adsh &&
-      //       presentation.stmt === "CP" &&
-      //       presentation.tag === financial.tag
-      //     );
-      //   });
-      //   return presentation.length > 0;
-      // });
-
-      // //filter financials to only include current quarter
-      // let currentQuarter = "20210630";
-      // let currentQuarterFinancials = financials.filter((financial) => {
-      //   return financial.ddate === currentQuarter;
-      // });
-
-      // //add presentation detail as a key-value pair of each financial object
-      // currentQuarterFinancials = currentQuarterFinancials.map((financial) => {
-      //   let presentation = company.presentations.filter((presentation) => {
-      //     return (
-      //       presentation.adsh === financial.adsh &&
-      //       presentation.stmt === "CP" &&
-      //       presentation.tag === financial.tag
-      //     );
-      //   });
-      //   if (presentation.length > 0) {
-      //     financial.presentation = presentation;
-      //   } else {
-      //     financial.presentation = [{ line: Infinity }];
-      //   }
-      //   return financial;
-      // });
-      // //sort the current quarter financials based on order of appearance in the income statement
-      // currentQuarterFinancials = currentQuarterFinancials.sort(
-      //   (x, y) => x.presentation[0].line - y.presentation[0].line
-      // );
-
-      // //remove all current quarter financials without a specified line on the income statement
-      // currentQuarterFinancials = currentQuarterFinancials.filter(
-      //   (financial) => financial.presentation[0].line !== Infinity
-      // );
 
       let currentQuarterFinancials = filterFinancials(company, 'CP', '20210630', null)
 
