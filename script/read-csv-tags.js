12b0c6b665dfaa637e514d350a51f1d2ca230cc2
Franklin Yin <franklinyin1@gmail.com>
2022-01-13 17:16:22 -0500

    script: read debt-tags csv

--- script/read-csv-tags.js
+++ script/read-csv-tags.js
@@ -5,6 +5,7 @@ const fs = require("fs")
 async function readCSVTags() {
 
   //read cash-tags
+
   const cashTagsCSV = await fsPromises.readFile("./csv/cash/cash-tags.csv");
   const parsedCashTags = parse(cashTagsCSV);
 
@@ -21,16 +22,137 @@ async function readCSVTags() {
     return cashTag !== undefined
   })
 
-  const path = "./cash-tags.txt"
+  const cashTagsPath = "./tags/cash-tags.txt"
 
-  if (fs.existsSync(path)) {
-    await fsPromises.rm(path)
+  if (fs.existsSync(cashTagsPath)) {
+    await fsPromises.rm(cashTagsPath)
   }
 
-  let stream = fs.createWriteStream("cash-tags.txt", {flags:'a'})
-  stream.write(JSON.stringify(cashTags))
-  stream.end()
+  let cashTagsStream = fs.createWriteStream(cashTagsPath, {flags:'a'})
+  cashTagsStream.write(JSON.stringify(cashTags))
+  cashTagsStream.end()
   console.log('extracted cash-tags')
+
+  //read debt-tags
+
+  const bondTagsCSV = await fsPromises.readFile("./csv/debt/debt-tags.csv")
+  const parsedBondTags = parse(bondTagsCSV)
+  parsedBondTags.shift()
+  let bondTags = parsedBondTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  bondTags = bondTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const borrowTagsCSV = await fsPromises.readFile("./csv/debt/borrow-tags.csv")
+  const parsedBorrowTags = parse(borrowTagsCSV)
+  parsedBorrowTags.shift()
+  let borrowTags = parsedBorrowTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  borrowTags = borrowTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const debtTagsCSV = await fsPromises.readFile("./csv/debt/debt-tags.csv")
+  const parsedDebtTags = parse(debtTagsCSV)
+  parsedDebtTags.shift()
+  let debtTags = parsedDebtTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  debtTags = debtTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const facilitiesTagsCSV = await fsPromises.readFile("./csv/debt/facilities-tags.csv")
+  const parsedFacilitiesTags = parse(facilitiesTagsCSV)
+  parsedFacilitiesTags.shift()
+  let facilitiesTags = parsedFacilitiesTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  facilitiesTags = facilitiesTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const facilityTagsCSV = await fsPromises.readFile("./csv/debt/facility-tags.csv")
+  const parsedFacilityTags = parse(facilityTagsCSV)
+  parsedFacilityTags.shift()
+  let facilityTags = parsedFacilityTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  facilityTags = facilityTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const lineTagsCSV = await fsPromises.readFile("./csv/debt/line-tags.csv")
+  const parsedLineTags = parse(lineTagsCSV)
+  parsedLineTags.shift()
+  let lineTags = parsedLineTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  lineTags = lineTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const loanTagsCSV = await fsPromises.readFile("./csv/debt/loan-tags.csv")
+  const parsedLoanTags = parse(loanTagsCSV)
+  parsedLoanTags.shift()
+  let loanTags = parsedLoanTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  loanTags = loanTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const mortgageTagsCSV = await fsPromises.readFile("./csv/debt/mortgage-tags.csv")
+  const parsedMortgageTags = parse(mortgageTagsCSV)
+  parsedMortgageTags.shift()
+  let mortgageTags = parsedMortgageTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  mortgageTags = mortgageTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const noteTagsCSV = await fsPromises.readFile("./csv/debt/note-tags.csv")
+  const parsedNoteTags = parse(noteTagsCSV)
+  parsedNoteTags.shift()
+  let noteTags = parsedNoteTags.map(tag => {
+    if (tag[2] === '1') {
+      return tag[0]
+    }
+  })
+  noteTags = noteTags.filter(tag => {
+    return tag !== undefined
+  })
+
+  const debtTagsPath = "./tags/debt-tags.txt"
+
+  if (fs.existsSync(debtTagsPath)) {
+    await fsPromises.rm(debtTagsPath)
+  }
+
+  let debtTagsStream = fs.createWriteStream(debtTagsPath, {flags:'a'})
+  debtTagsStream.write(JSON.stringify(debtTags))
+  debtTagsStream.end()
+  console.log('extracted debt-tags')
 
 }
 
