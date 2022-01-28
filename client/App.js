7bb928778a516a70801691154efee0d55958eb61
Franklin Yin <franklinyin1@gmail.com>
2022-01-28 18:00:11 -0500

    feature: ErrorBoundary

--- client/App.js
+++ client/App.js
@@ -1,31 +1,31 @@
 import React from "react";
 
 import Routes from "./Routes";
-
+import ErrorBoundary from "./components/ErrorBoundary";
 import Layout from "./components/Layout";
 
 import { createTheme, ThemeProvider } from "@material-ui/core";
 
-import { purple } from '@material-ui/core/colors'
-
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#E7ECEF'
+      main: "#E7ECEF",
     },
     secondary: {
-      main: '#274C77'
+      main: "#274C77",
     },
-  }
+  },
 });
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <Routes />
-      </Layout>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Routes />
+        </Layout>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
 
